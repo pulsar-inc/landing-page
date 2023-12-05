d719c4671cde2ed4f776fa08001340548bc593b7
Arnaud Gissinger <agissing@student.42.fr>
2023-12-05 09:48:46 +0100

    [ADD] nuxt3 base

---
+++ app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  title: 'Coming soon.',
+  description: 'Pulsar official website.',
+  ogImage: 'https://i.ibb.co/z68VQz4/og-soon.png',
+})
---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  // head: {
+  //   title,
+  //   meta: [
+  //     { charset: 'utf-8' },
+  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //     { hid: 'og:title', property: 'og:title', content: title },
+  //     { hid: 'description', name: 'description', content: description },
+  //     { hid: 'og:desc', property: 'og:description', content: description },
+  //     { hid: 'og:image', property: 'og:image', content: image },
+  //     { name: 'theme-color', content: '#ffffff' }
+  //   ],
+  //   link: [
+  //     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  //     { name: 'msapplication-TileColor', content: '#2b5797' },
+  //     { rel: 'shortcut icon', type: 'image/png', href: '/logo.png' },
+  //     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Muli:wght@300;400;600;700&display=swap' },
+  //     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+  //     { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+  //   ]
+  // },
+    /*
+  ** PWA config
+  */
+  // pwa: {
+  //   manifest: {
+  //     name: 'Pulsar',
+  //     short_name: 'Pulsar',
+  //     display: 'standalone',
+  //     theme_color: '#17171E',
+  //     background_color: '#17171E'
+  //   },
+  //   icon: {
+  //     iconSrc: '~/static/logo.png',
+  //     iconFileName: 'logo.png'
+  //   }
+  // },
+})
